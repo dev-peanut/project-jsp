1068be590ce69f12084f2599ea56169775bb891a
dev-mwYoon <dev.mwYoon@gmail.com>
2023-02-28 13:44:24 +0900

    프로젝트명 변경

---
+++ workspace/dhouse/src/com/dhouse/file/FileFrontController.java
@@ -0,0 +1,26 @@
+package com.dhouse.file;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dhouse.Result;
+
+public class FileFrontController extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String uri = req.getRequestURI();
+		String contextPath = req.getContextPath();
+		String target = uri.replace(contextPath + "/", "").split("\\.")[0];
+		Result result = null;
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doPost(req, resp);
+	}
+}

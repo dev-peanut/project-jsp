092468619bd36286fed7d2b85cb29f78ecd08642
dev-sein <seinee114@gmail.com>
2023-03-03 19:20:22 +0900

    마이페이지 js

--- workspace/dhouse/WebContent/static/js/user/payment.js
+++ workspace/dhouse/WebContent/static/js/user/payment.js
@@ -1,2 +1,22 @@
-/*payment*/
+/**
+ * 
+ */
+/*전체 체크 박스*/
+
+
+// 체크박스 개별 선택
+
+
+const $Checkbox_icon = $(".Checkbox_icon");
+var check=false;
+
+$(".Checkbox_icon").on("click", $(".Checkbox_icon"), function(){
+	if(check){
+	$("#checkboxmint").css('opacity','1');
+	$("#checkboxmint").css('backgroundColor', '#00c4c4');}
+	else{
+		
+	}
+	
+});
 

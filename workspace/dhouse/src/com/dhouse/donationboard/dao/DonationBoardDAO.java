64758abdbdce5ce634287d646639759101f88e24
SeyunKimdev <seyun.dev@gmail.com>
2023-03-07 17:32:43 +0900

    김세윤 DonationBoardDAO 수정

--- workspace/dhouse/src/com/dhouse/donationboard/dao/DonationBoardDAO.java
+++ workspace/dhouse/src/com/dhouse/donationboard/dao/DonationBoardDAO.java
@@ -15,40 +15,47 @@ public class DonationBoardDAO {
 	public DonationBoardDAO() {
 		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
 	}
+//	게시글 목록
+	public List<DonationBoardDTO> selectAll(Map<String, Object> pageMap){
+		return sqlSession.selectList("donationBoard.selectAll", pageMap);
+	}
 	
-//		게시글 목록
-		public List<DonationBoardDTO> selectAll(Map<String, Object> pageMap){
-			return sqlSession.selectList("donationBoard.selectAll", pageMap);
-		}
-		
-//		게시글 총 개수
-		public Long getTotal(Map<String, Object> searchMap) {
-			return sqlSession.selectOne("donationBoard.getTotal", searchMap);
-		}
-		
-//		게시글 조회
-		public DonationBoardDTO select(Long donationBoardId) {
-			return sqlSession.selectOne("donationBoard.select", donationBoardId);
-		}
-		
-//		게시글 추가
-		public void insert(DonationBoardVO donationBoardVO) {
-			sqlSession.insert("donationBoard.insert", donationBoardVO);
-		}
-		
-//		현재 시퀀스 조회
-		public Long getCurrentSequence() {
-			return sqlSession.selectOne("donationBoard.getCurrentSequence");
-		}
-		
-////		게시글 수정
-//		public void update(BoardVO boardVO) {
-//			System.out.println(boardVO);
-//			sqlSession.update("board.update", boardVO);
-//		}
+//	게시글 총 개수
+	public Long getTotal(Map<String, Object> searchMap) {
+		return sqlSession.selectOne("donationBoard.getTotal", searchMap);
+	}
+	
+//	게시글 조회
+	public DonationBoardDTO select(Long donationBoardId) {
+		return sqlSession.selectOne("donationBoard.select", donationBoardId);
+	}
+	
+//	게시글 추가
+	public void insert(DonationBoardVO donationBoardVO) {
+		sqlSession.insert("donationBoard.insert", donationBoardVO);
+	}
+	
+//	현재 시퀀스 조회
+	public Long getCurrentSequence() {
+		return sqlSession.selectOne("donationBoard.getCurrentSequence");
+	}
+	
+////	게시글 수정
+//	public void update(BoardVO boardVO) {
+//		System.out.println(boardVO);
+//		sqlSession.update("board.update", boardVO);
+//	}
 //
-//		public void delete(Long boardId) {
-//			sqlSession.delete("board.delete", boardId);
-//		}
+//	public void delete(Long boardId) {
+//		sqlSession.delete("board.delete", boardId);
 	
+//	메인페이지에 띄울 랭킹
+	public List<DonationBoardDTO> selectRanking(){
+		return sqlSession.selectList("donationBoard.selectRanking");
+	}
+	
+//	메인페이지에 띄울 최신글
+	public List<DonationBoardDTO> selectList(){
+		return sqlSession.selectList("donationBoard.selectList");
+	}
 }

93a5094dd710f3f9ad1a3ae1a8a649864e37b0fc
dev-mwYoon <dev.mwYoon@gmail.com>
2023-03-08 13:15:41 +0900

    AdminFoodDeleteOkController 작업 file 삭제 추가

--- workspace/dhouse/src/com/dhouse/admin/AdminFoodDeleteOkController.java
+++ workspace/dhouse/src/com/dhouse/admin/AdminFoodDeleteOkController.java
@@ -10,25 +10,21 @@ import javax.servlet.http.HttpServletResponse;
 import com.dhouse.Action;
 import com.dhouse.Result;
 import com.dhouse.admin.dao.AdminDAO;
-import com.dhouse.file.dao.FileDAO;
 
 public class AdminFoodDeleteOkController implements Action {
 
 	@Override
 	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		AdminDAO adminDAO = new AdminDAO();
-		FileDAO fileDAO = new FileDAO();
 		Result result = new Result();
 		Long foodId = Long.valueOf(req.getParameter("foodId"));
 		String uploadPath = req.getSession().getServletContext().getRealPath("/") + "upload/";
 		
-		fileDAO.selectFood(foodId).stream().map(file -> new File(uploadPath + file.getFileSystemName())).forEach(file -> {
-			if(file.exists()) {
-				file.delete();
-			}
-		});
+		File file = new File(uploadPath + adminDAO.selectFood(foodId).getFileSystemName());
+		if(file.exists()) {
+			file.delete();
+		}
 		
-		fileDAO.deleteFood(foodId);
 		adminDAO.deleteFood(foodId);
 		result.setPath(req.getContextPath() + "/admin/food/list.admin");
 		result.setRedirect(true);
